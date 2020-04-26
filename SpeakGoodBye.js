a9d1ca04c36144bb1ff16327bf512a4f18a358b4
hemant3454 <63035764+hemant3454@users.noreply.github.com>
2020-04-27 00:24:24 +0530

    JS

---
+++ SpeakGoodBye.js
@@ -0,0 +1,9 @@
+
+var speakWord = "Good Bye";
+var byeSpeaker={};
+
+byeSpeaker.speak=function(name) {
+  console.log(speakWord + " " + name);
+}
+
+
---
+++ SpeakHello.js
@@ -0,0 +1,10 @@
+
+
+var speakWord1 = "Hello";
+var helloSpeaker={};
+
+helloSpeaker.speak=function(name) {
+  console.log(speakWord1 + " " + name);
+}
+
+
